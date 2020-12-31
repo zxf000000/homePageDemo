92936dfbf9dde9d4749b5ddd9c10f733b09ac7db
zhouxiaofei <zhouxiaofei0001@gmail.com>
2020-12-31 15:16:16 +0800

    initilize commit

---
+++ src/components/Another.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container">
+    <div class="container__sub">
+      <div class="container__sub--first"></div>
+    </div>
+    <div class="other">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Another',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="sass">
+
+$backgroundColor: red
+$container-width: 100px
+$sub-width: 90px
+$first-width: 80px
+
+%shadow
+  box-shadow: 0 0 20px black
+
+.other
+  margin-top: 20px
+  width: 200px
+  height: 200px
+  border-radius: 20px
+  border: dashed 1px #fff
+  background: linear-gradient(white, white) padding-box, linear-gradient(to right, blue, yellow) border-box
+
+.container
+  margin-left: 100px
+  background: $backgroundColor
+  width: $container-width
+  height: $container-width
+  border-radius: $container-width/2
+  @extend %shadow
+  &__sub
+    background: blue
+    width: $sub-width
+    height: $sub-width
+    border-radius: $sub-width/2
+    &--first
+      background: yellow
+      width: $first-width
+      height: $first-width
+      border-radius: $first-width/2
+
+
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,114 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container class="d-flex flex-column container">
+    <div class="d-flex buttons">
+      <v-btn v-on:click="tapLeft" :disabled="showA">Enterprise</v-btn>
+      <v-btn v-on:click="tapRight" :disabled="!showA">Individual</v-btn>
+    </div>
+    <div class="page-box">
+      <div class="page-container" :class="{'slide-in': showA}">
+        <div class="big-box-a big-box"></div>
+        <div class="small-box-a small-box"></div>
+      </div>
+      <div class="page-container" :class="{'slide-in': !showA}">
+        <div class="big-box-b big-box"></div>
+        <div class="small-box-b small-box"></div>
+      </div>
+    </div>
+    <div style="height: 1000px"></div>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  export default {
+    name: 'HelloWorld',
+    methods: {
+      tapLeft: function () {
+        this.showA = true
+      },
+      tapRight: function () {
+        this.showA = false
+      }
+    },
+    mounted() {
+      let thresholds = []
+      for (let i = 0; i < 100; i++) {
+        thresholds.push(i * 0.01)
+      }
+      let options = {
+        rootMargin: '0px',
+        threshold: thresholds
+      }
+      let observer = new IntersectionObserver((entries) => {
+        let entry = entries[0]
+        console.log(entry)
+
+          console.log(entry.intersectionRatio)
+          let minTop = 400;
+          let maxTop = 700;
+          let currentTop = (maxTop - minTop) * (1 - entry.intersectionRatio) + minTop
+          let targets = document.getElementsByClassName('small-box')
+          console.log(currentTop)
+          for (var i = 0; i < targets.length; i++) {
+            targets[i].setAttribute('style', 'top:' + currentTop + 'px !important');
+          }
+
+
+      console.log(window.innerHeight)
+      }, options)
+      observer.observe(document.querySelector('.page-container'))
+    },
+    data: () => ({
+      showA: false
+    }),
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style scoped lang="sass">
+  $page-width: 800px
+  $page-height: 1000px
+  $transition: all 0.5s linear
+  .container
+    margin-top: 1000px
+
+  .buttons
+    height: 100px
+    background: aquamarine
+
+  .page-container
+    position: absolute
+    width: $page-width
+    height: $page-height
+    margin-top: 30px
+    transition: $transition
+    transform: translateX(10%)
+    opacity: 0
+    background: gray
+
+  .slide-in
+    transform: translateX(0)
+    opacity: 1
+
+  .big-box
+    width: 800px
+    height: 500px
+
+  .big-box-a
+    background: blue
+
+  .big-box-b
+    background: red
+
+  .small-box
+    width: 250px
+    height: 200px
+    position: absolute
+    left: 200px
+    top: 600px
+
+  .small-box-a
+    background: yellow
+
+  .small-box-b
+    background: green
+
 </style>
