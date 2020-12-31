323fa892bbeb09fe2621b5d22f010c480115be01
zhouxiaofei <zhouxiaofei0001@gmail.com>
2020-12-31 16:56:35 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,6 @@
 <template>
   <v-container class="d-flex flex-column container">
+    <div style="height: 500px"></div>
     <div class="d-flex buttons">
       <v-btn v-on:click="tapLeft" :disabled="showA">Enterprise</v-btn>
       <v-btn v-on:click="tapRight" :disabled="!showA">Individual</v-btn>
@@ -7,14 +8,14 @@
     <div class="page-box">
       <div class="page-container" :class="{'slide-in': showA}">
         <div class="big-box-a big-box"></div>
-        <div class="small-box-a small-box"></div>
+        <div class="small-box-a small-box" :class="{'small-slide-out': !showA}"></div>
       </div>
       <div class="page-container" :class="{'slide-in': !showA}">
         <div class="big-box-b big-box"></div>
-        <div class="small-box-b small-box"></div>
+        <div class="small-box-b small-box" :class="{'small-slide-out': showA}"></div>
       </div>
     </div>
-    <div style="height: 1000px"></div>
+    <div style="height: 2000px"></div>
   </v-container>
 </template>
 
@@ -30,32 +31,27 @@
       }
     },
     mounted() {
-      let thresholds = []
-      for (let i = 0; i < 100; i++) {
-        thresholds.push(i * 0.01)
-      }
-      let options = {
-        rootMargin: '0px',
-        threshold: thresholds
-      }
-      let observer = new IntersectionObserver((entries) => {
-        let entry = entries[0]
-        console.log(entry)
-
-          console.log(entry.intersectionRatio)
-          let minTop = 400;
-          let maxTop = 700;
-          let currentTop = (maxTop - minTop) * (1 - entry.intersectionRatio) + minTop
-          let targets = document.getElementsByClassName('small-box')
-          console.log(currentTop)
-          for (var i = 0; i < targets.length; i++) {
-            targets[i].setAttribute('style', 'top:' + currentTop + 'px !important');
-          }
-
-
-      console.log(window.innerHeight)
-      }, options)
-      observer.observe(document.querySelector('.page-container'))
+      console.log('enter')
+      window.addEventListener('scroll', function () {
+        let startOffet = 0
+        let endOffset = 1200
+        let windowOffset = window.pageYOffset
+        let progress = 0
+        if (windowOffset > endOffset) {
+          progress = 1
+        } else if (windowOffset < startOffet) {
+          progress = 0
+        } else {
+          progress = (windowOffset - startOffet)/1200
+        }
+        let smallStartOffset = 700
+        let smallBoxEndOffset = 0
+        let currentOffset = smallStartOffset - progress * (smallStartOffset - smallBoxEndOffset)
+        let targets = document.getElementsByClassName('small-box')
+        for (var i = 0; i < targets.length; i++) {
+          targets[i].setAttribute('style', 'top:' + currentOffset + 'px !important');
+        }
+      })
     },
     data: () => ({
       showA: false
@@ -65,10 +61,8 @@
 
 <style scoped lang="sass">
   $page-width: 800px
-  $page-height: 1000px
-  $transition: all 0.5s linear
-  .container
-    margin-top: 1000px
+  $page-height: 500px
+  $transition: all 0.2s linear
 
   .buttons
     height: 100px
@@ -104,11 +98,15 @@
     position: absolute
     left: 200px
     top: 600px
+    transition: transform 0.2s linear
 
   .small-box-a
     background: yellow
 
   .small-box-b
     background: green
+
+  .small-slide-out
+    transform: translate(40%, -40%)
 
 </style>
